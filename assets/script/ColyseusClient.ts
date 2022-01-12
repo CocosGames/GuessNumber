4d39c2ddf2a38febc423b80fb5721fff69c59ae6
COCO <support@cocosgames.com>
2022-01-12 16:53:00 +0800

    1st commit

---
+++ assets/script/ColyseusClient.ts
@@ -0,0 +1,43 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+import Colyseus from 'db://colyseus-sdk/colyseus.js';
+
+@ccclass('NetworkManager')
+export class NetworkManager extends Component {
+    @property hostname = "localhost";
+    @property port = 2567;
+    @property useSSL = false;
+
+    client!: Colyseus.Client;
+    room!: Colyseus.Room;
+
+    start () {
+        // 实例化 Colyseus 客户端
+        // 连接到 (ws|wss)://hostname[:port]
+        this.client = new Colyseus.Client(`${this.useSSL ? "wss" : "ws"}://${this.hostname}${([443, 80].includes(this.port) || this.useSSL) ? "" : `:${this.port}`}`);
+
+        // 连接到 room
+        this.connect();
+    }
+
+    async connect() {
+        try {
+            this.room = await this.client.joinOrCreate("my_room");
+
+            console.log("joined successfully!");
+            console.log("user's sessionId:", this.room.sessionId);
+
+            this.room.onStateChange((state) => {
+                console.log("onStateChange: ", state);
+            });
+
+            this.room.onLeave((code) => {
+                console.log("onLeave:", code);
+            });
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
